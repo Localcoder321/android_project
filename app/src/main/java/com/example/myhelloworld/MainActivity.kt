50d77288b5719e9824b896a5e1cacf0099ebc009
Localcoder <wiut00011625@gmail.com>
2023-10-06 12:37:23 +0500

    Session 4, adding images

--- app/src/main/java/com/example/myhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/myhelloworld/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myhelloworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,7 +20,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -25,6 +31,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.myhelloworld.ui.theme.MyHelloWorldTheme
 import models.Movie
 
@@ -53,6 +60,7 @@ fun DetailedView(movie: Movie) {
             .fillMaxWidth()
             .padding(16.dp)
     ) {
+        MyImage()
         Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.Bottom) {
             Name(name = movie.name)
             Spacer(Modifier.weight(1f))
@@ -62,6 +70,19 @@ fun DetailedView(movie: Movie) {
         MyDivider()
         Spacer(Modifier.height(16.dp))
         Actors(actors = movie.actors)
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.Bottom) {
+            Image2()
+            Image2()
+            Image2()
+            Image2()
+            Image2()
+            Image2()
+            Image2()
+            Image2()
+            Image2()
+            Image2()
+            Text(text = "10 drunk people from 10")
+        }
     }
 }
 
@@ -137,5 +158,35 @@ private fun Budget(budget: Int) {
         color = Color.Black,
         fontSize = 15.sp,
         fontFamily = FontFamily.SansSerif
+    )
+}
+
+
+@Composable
+fun MyImage () {
+   AsyncImage(
+       model = "https://assets.mubicdn.net/images/artworks/617909/images-original.png?1689617236",
+       placeholder = painterResource(
+           id = R.drawable.dragon_black
+       ),
+       error = painterResource(
+           id = R.drawable.dragon_black
+       ),
+       contentDescription = "STUDY",
+       contentScale = ContentScale.Crop,
+       modifier = Modifier
+           .size(350.dp)
+           .padding(20.dp)
+           .clip(RoundedCornerShape(20.dp))
+   )
+}
+
+@Composable
+fun Image2(){
+    Image(
+        painter = painterResource(
+            id = R.drawable.baseline_local_drink_24),
+        contentDescription = stringResource(
+        id = R.string.app_name)
     )
 }
